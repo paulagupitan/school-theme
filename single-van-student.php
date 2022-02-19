4750f09d610fb5bfb1fe5e6066c315f9ccd582d0
Paul Agupitan <paulagupitan@gmail.com>
2022-02-18 22:48:53 -0800

    updated

--- single-van-student.php
+++ single-van-student.php
@@ -13,68 +13,65 @@ get_header();
 <!-- <main id="primary" class="site-main"> -->
 
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="single-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+<main id="primary" class="site-main">
+    <!-- 16. Create Single Student Page, then go to content-fwd29school-student to create taxonomy -->
+    <?php
+		while ( have_posts() ) :
+			the_post();
 
-		?>
-    </header><!-- .entry-header -->
-    <div class="entry-content">
-        <div class="alignright">
-            <?php 
-			
-			the_post_thumbnail('medium');
-			the_content();
+			// 19. Add the single student content from "taxonomy-fwd29school-student.php"
 			?>
+    <article class="student-info">
+        <a href="<?php the_permalink(); ?>">
+            <h2><?php the_title(); ?></h2>
+        </a>
+        <div class="alignright">
+            <?php the_post_thumbnail( 'medium' ); ?>
         </div>
-        <div class="meet-others">
-            <h3>Meet Other Designer Students:</h3>
-            <?php
-				$terms = get_the_terms( $post->ID, 'van-student-category' );
-				if ( $terms && ! is_wp_error( $terms ) ) {
-					foreach ( $terms as $term ) {
-						$args = array(
-							'post_type'      => 'van-student',				
-							'posts_per_page' => -1,		
-							'post__not_in'	=>	array ($post->ID, 'van-student-category'),
-							'order'          => 'ASC',				
-							'orderby'        => 'title',	
-							'tax_query'		=> array(
-								array(
-									'taxonomy'  => 'van-student-category',
-									'field'		=> 'slug',
-									'terms'		=> $term->slug
-								)
-							)		
-						);		
-						$query = new WP_Query( $args );							 				
-						if ( $query -> have_posts() ) {	
-							?>
-            <?php
-							while ( $query -> have_posts() ) {				
-								$query -> the_post();	
-								?>
-            <div class="other-students">
-                <?php			
-								echo '<a href="'. get_permalink() .'">'. get_the_title() .'</a>';				
-							}		
-							wp_reset_postdata();
-								?>
-            </div>
-            <?php								 
-						}
-					}
-				}
-				?>
-        </div>
+        <?php the_content(); ?>
+    </article>
 
-    </div><!-- .entry-content -->
-</article><!-- #post-<?php the_ID(); ?> -->
+    <aside>
+        <?php
+					$terms = get_the_terms( $post->ID, 'van-student-category' );
+					$term  = array_shift( $terms );
+					?>
+        <h2>Meet Other <?php echo $term->name; ?> Student</h2>
+        <?php
+							$args = array(
+								'post_type' 	=> 'van-student',
+								'post_per_page' => -1,
+								'post__not_in' 	=> array ( $post->ID ),
+								'orderby'		=> 'title',
+								'order'			=> 'ASC',
+								'tax_query'		=> array(
+									array(
+										'taxonomy'	=> 'van-student-category',
+										'field'		=> 'slug',
+										'terms'		=> $term->slug
+									)
+								)	
+							);
+							$query = new WP_Query( $args );
+							if ( $query -> have_posts() ) {
+								while ( $query -> have_posts() ) {
+									$query -> the_post();
+									?>
+        <a href=" <?php the_permalink(); ?>">
+            <h3> <?php the_title(); ?> </h3>
+        </a>
+        <?php
+								}
+								wp_reset_postdata();
+							}
+					?>
+    </aside>
+
+    <?php
+		endwhile; // End of the loop.
+		?>
+
+</main><!-- #main -->
 
 <?php
 get_footer();
